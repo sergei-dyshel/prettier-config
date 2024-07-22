70c988a91edc9dce3918034fa5f804c36d3d9fc9
Sergei Dyshel <sergei@dyshel.com>
2024-07-22 12:26:17 +0300

    +

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // @ts-check
 
-/** @type {import("prettier").Config} */
+/** @type {import("prettier").Config & import("prettier-plugin-jsdoc").JsdocOptions} */
 module.exports = {
   // must preserve order of plugins, otherwise jsdoc plugin is not working
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-jsdoc"],
   printWidth: 100,
+  jsdocCommentLineStrategy: "keep",
+  proseWrap: "always",
 };
